86da70daa0ec3a4f62148d487aa3dfea511538b2
FelipeFsc <felipefsc.04@gmail.com>
2025-06-25 01:36:15 -0300

    Modelo inicial do tracker mobile

--- tracker-web/src/app/home/home.ts
+++ tracker-web/src/app/home/home.ts
@@ -1,5 +1,5 @@
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
     selector: 'app-home',
@@ -7,8 +7,13 @@ import { Component } from '@angular/core';
     styleUrl: './home.css',
     standalone: false
 })
-export class Home {
+export class Home implements AfterViewChecked {
+
+    @ViewChildren('lastTask') lastTasks!: QueryList<ElementRef>;
+
     isMobile = false;
+
+    tasks: any[] = [];
 
     constructor(private breakpointObserver: BreakpointObserver) {
         this.breakpointObserver
@@ -18,12 +23,30 @@ export class Home {
             });
     }
 
-    tasks: any[] = [
-        { title: '', date: '', duration: 0 }
-    ];
+    ngAfterViewChecked() {
+        if (this.lastTasks && this.lastTasks.length > 0) {
+            const lastTaskElement = this.lastTasks.last.nativeElement;
+            lastTaskElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
 
     adicionarTarefa() {
-        this.tasks.push({ title: '', date: '', duration: 0 });
+        const agora = new Date();
+        const horaAtual = agora.toTimeString().slice(0, 5); // "HH:mm"
+
+        const ultimaTarefa = this.tasks.length > 0 ? this.tasks[this.tasks.length - 1] : null;
+
+        // Atualiza horaFim da última tarefa, se existir
+        if (ultimaTarefa) {
+            ultimaTarefa.horaFim = horaAtual;
+        }
+
+        // Cria a nova tarefa, copiando a descrição da última se existir
+        this.tasks.push({
+            horaInicio: horaAtual,
+            horaFim: '',
+            descricao: ultimaTarefa ? ultimaTarefa.descricao : '',
+        });
     }
 
     removerTarefa(index: number) {
