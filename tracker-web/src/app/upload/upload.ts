e8169eca496a8ec50d892442141981ccbf844d04
FelipeFsc <felipefsc.04@gmail.com>
2025-07-28 22:20:47 -0300

    Adicionando a primeira versão da leitura do excel no upload

--- tracker-web/src/app/upload/upload.ts
+++ tracker-web/src/app/upload/upload.ts
@@ -1,5 +1,7 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import * as XLSX from 'xlsx';
 
 @Component({
     selector: 'app-upload',
@@ -33,7 +35,10 @@ export class Upload {
 
     selectedFiles: File[] = [];
 
+    fileSelectado: any;
+
     constructor(
+        private datePipe: DatePipe,
         private router: Router
     ) { }
 
@@ -68,9 +73,12 @@ export class Upload {
     onFileSelect(event: Event) {
         const input = event.target as HTMLInputElement;
         if (input.files) {
-            this.handleFiles(input.files);
-                this.selectedFiles = Array.from(input.files);
 
+            this.fileSelectado = event.target;
+
+
+            this.handleFiles(input.files);
+            this.selectedFiles = Array.from(input.files);
         }
     }
 
@@ -79,5 +87,58 @@ export class Upload {
             console.log('Arquivo selecionado:', file);
             // Aqui você pode enviar o arquivo para backend usando um service
         });
+    }
+
+
+    data: any;
+
+    onVerifyFile() {
+        const reader = new FileReader();
+
+        reader.onload = (e: any) => {
+            const binaryData = e.target.result;
+            const workbook = XLSX.read(binaryData, { type: 'binary', dateNF: 'yyyy-mm-dd' });
+            const sheetName = workbook.SheetNames[0];
+            const workSheet = workbook.Sheets[sheetName];
+            const selectdData = XLSX.utils.sheet_to_json(workSheet, { header: 1 });
+
+            this.data = selectdData.map((row: any) => {
+                let miliseconds = (row[this.fields[1].column] - 1) * 24 * 60 * 60 * 1000;
+                const date = new Date(1900, 0, 1);
+                date.setTime(date.getTime() + miliseconds);
+
+                var item = {
+                    colaborador: row[this.fields[0].column],
+                    data: this.datePipe.transform(date, 'MM/dd/yyyy'),
+                    horaInicio: this.excelTimeToString(row[this.fields[2].column]),
+                    horaFim: this.excelTimeToString(row[this.fields[3].column]),
+                    tipoAtividade: row[this.fields[4].column],
+                    solicitante: row[this.fields[5].column],
+                    os: row[this.fields[6].column],
+                    observacoes: row[this.fields[7].column]
+                }
+
+                return item;
+            });
+            console.log(this.data);
+        }
+
+        reader.readAsArrayBuffer(this.fileSelectado.files[0]);
+    }
+
+    excelTimeToString(value: number): string {
+        const totalSeconds = Math.round(value * 24 * 60 * 60);
+
+        const hours = Math.floor(totalSeconds / 3600)
+            .toString()
+            .padStart(2, '0');
+
+        const minutes = Math.floor((totalSeconds % 3600) / 60)
+            .toString()
+            .padStart(2, '0');
+
+        const seconds = (totalSeconds % 60).toString().padStart(2, '0');
+
+        return `${hours}:${minutes}:${seconds}`;
     }
 }
